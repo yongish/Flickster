ff3a716f3e807edb779d8d963aaf5c5477ae0d9e
zhiyong tan <zhiyong@uber.com>
2016-07-24 17:43:35 -0700

    commented

--- app/src/main/java/com/codepath/flickster/MovieActivity.java
+++ app/src/main/java/com/codepath/flickster/MovieActivity.java
@@ -62,6 +62,26 @@ public class MovieActivity extends AppCompatActivity {
     }
 
     public void playTrailer(View v) {
+        // trailers.size() is 0 if the code below is uncommented. It should be 6.
+//        String url = "https://api.themoviedb.org/3/movie/209112/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+//        client.get(url, new JsonHttpResponseHandler(){
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+//                JSONArray trailersJsonResults;
+//                try {
+//                    trailersJsonResults = response.getJSONArray("results");
+//                    trailers.addAll(Trailer.fromJSONArray(trailersJsonResults));
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+//                super.onFailure(statusCode, headers, responseString, throwable);
+//            }
+//        });
+
         Intent video = new Intent(MovieActivity.this, VideoActivity.class);
         Movie i = (Movie) lvItems.getItemAtPosition(lvItems.getPositionForView(v));
         video.putExtra("id", i.getId());
@@ -114,14 +134,15 @@ public class MovieActivity extends AppCompatActivity {
             }
         });
 
+        // trailers.size() is 6 if the code below is uncommented.
 //        url = "https://api.themoviedb.org/3/movie/209112/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 //        client.get(url, new JsonHttpResponseHandler(){
 //            @Override
 //            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                JSONArray movieJsonResults;
+//                JSONArray trailersJsonResults;
 //                try {
-//                    movieJsonResults = response.getJSONArray("results");
-//                    trailers.addAll(Trailer.fromJSONArray(movieJsonResults));
+//                    trailersJsonResults = response.getJSONArray("results");
+//                    trailers.addAll(Trailer.fromJSONArray(trailersJsonResults));
 //                } catch (JSONException e) {
 //                    e.printStackTrace();
 //                }
